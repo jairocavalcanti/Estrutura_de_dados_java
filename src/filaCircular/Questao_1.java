0d85dbeade58327fdc6a1a9d65c1f5a59dac69fd
Jairo <jairoregiscavalcanti@gmail.com>
2024-05-10 15:00:33 -0300

    Enunciado_questão_1_inserido

--- src/filaCircular/Questao_1.java
+++ src/filaCircular/Questao_1.java
@@ -2,7 +2,7 @@ package filaCircular;
 
 import dados.Item;
 
-/* */
+/* Faça um método que retire os valores negativos da fila circular (obedecendo sua propriedade) */
 
 public class Questao_1 {
     public static void main(String[] args) {
